a6371b12b1c62f3a9ed7b9e7635b3e4a5f403814
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-12-07 22:50:09 +0530

    Added MemoriesPage

---
+++ src/pages/MemoriesPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-title>Memories Page</ion-title>
+    </ion-header>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import { IonPage, IonHeader, IonTitle } from "@ionic/vue";
+
+export default {
+  components: {
+    IonPage,
+    IonHeader,
+    IonTitle,
+  },
+};
+</script>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,16 +1,14 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router';
 import { RouteRecordRaw } from 'vue-router';
-import Home from '../views/Home.vue'
-
+import MemoriesPage from '../pages/MemoriesPage.vue';
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    redirect: '/home'
+    redirect: '/memories'
   },
   {
-    path: '/home',
-    name: 'Home',
-    component: Home
+    path: '/memories',
+    component: MemoriesPage
   }
 ]
 
